bf511bd288076aa8d3d8083113b2dc70f083627c
Nebotxavi <nebotxavi@gmail.com>
2020-05-21 23:56:52 +0200

    Documentacion

--- calculadora/RPN.java
+++ calculadora/RPN.java
@@ -1,6 +1,16 @@
 package calculadora;
 
+/**
+ * Paquete para calcular el resultado de una operación cuyos parametros vienen dados en Reverse Polish notation.
+ * Reverse Polish notation (RPN) Notación Polaca inversa, por ejemplo
+ * la expresión: 5 + 5 nos da 10 respetando la prioridad de
+ * los operadores en notación RPN seria: 5 5 +
+ */
+
 class NodoPila {
+	/**
+	 * Clase que gestiona los atributos de la pila.
+	 */
 	protected NodoPila abajo;
 	protected double dato;
 	
@@ -11,10 +21,15 @@ class NodoPila {
 }
 
 public class RPN {
+	/**
+	 * Clase principal para el cálculo del resultado de una operación con parametros RPN.
+	 * @param String commando. Un string con los valores y el símbolo que define la operación. Ejemplo: "5 5 +". 
+	 */	
 	private String commando;
 	private NodoPila arriba;
 	
 	public RPN(String commando) {
+
 		arriba = null;
 		this.commando = commando;
 	}
@@ -31,6 +46,11 @@ public class RPN {
 	}
 	
 	private void pusher(String[] inputItems) {
+		/**
+		 * Método que separa los diferentes items contenidos en el string que recibe el constructor y envía como parámetros a los métodos correspondientes.
+		 * @param String[] 
+		 * @return void
+		 */
 		double a, b;
 				  
         for (String item : inputItems) {
@@ -61,6 +81,11 @@ public class RPN {
 	
 	
 	public double resultado() {
+		/**
+		 * Método publico que llama al resto de métodos.
+		 * @params none
+		 * @return double val. Número resultado de la operación introducida en el constructor.
+		 */
 		String[] inputItems = commando.split(" ");
 		pusher(inputItems);
 		double val = popPila();
--- calculadora/TestRPN.java
+++ calculadora/TestRPN.java
@@ -2,20 +2,10 @@ package calculadora;
 
 import java.util.Scanner;
 
-/*
-  Objetivo: Obtener código refactorizado a partir de otro que no lo está.
-  Tarea: Para esta tarea, se refactorizará un programa mal escrito, sin 
-  cambiar la forma en que funcionael programa. Este programa, RPN.java 
-  es una calculadora de notación inversa polaca que utiliza una pila.  
-  Reverse Polish notation (RPN) NotaciónPolacainversa, 
-  por ejemplo la expresión: 4 * 5 -7 / 2 % 3 nos da 1,5 respetando la prioridad de
-  los operadores en notación RPN seria: 4 5 * 7 2 / -3 % (pues no podemos 
-  poner los paréntesis para alterar la prioridad)
-  Se debe reorganizar este código usando almenos tres de las reglas vistas en clase.
-*/
-
 public class TestRPN {
-	/* metodo main */
+	/**
+	 * Clase para probar la clase RPN.
+	 */
 	public static void main(String[] args) {
 		Scanner in;
 		while(true) {
@@ -32,5 +22,4 @@ public class TestRPN {
 		}
 		in.close();
 	}
-
 }
