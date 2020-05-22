1cc11d15be493be5e447c64222b25fc60bf97eb6
Nebotxavi <nebotxavi@gmail.com>
2020-05-22 22:34:39 +0200

    Documentacion

--- calculadora/RPN.java
+++ calculadora/RPN.java
@@ -1,16 +1,6 @@
 package calculadora;
 
-/**
- * Paquete para calcular el resultado de una operación cuyos parametros vienen dados en Reverse Polish notation.
- * Reverse Polish notation (RPN) Notación Polaca inversa, por ejemplo
- * la expresión: 5 + 5 nos da 10 respetando la prioridad de
- * los operadores en notación RPN seria: 5 5 +
- */
-
 class NodoPila {
-	/**
-	 * Clase que gestiona los atributos de la pila.
-	 */
 	protected NodoPila abajo;
 	protected double dato;
 	
@@ -20,14 +10,21 @@ class NodoPila {
 	}
 }
 
+/**
+ * Paquete para calcular el resultado de una operación cuyos parametros vienen dados en Reverse Polish notation.
+ * Reverse Polish notation (RPN) Notación Polaca inversa, por ejemplo
+ * la expresión: 5 + 5 nos da 10 respetando la prioridad de
+ * los operadores en notación RPN seria: 5 5 +
+ */
+
 public class RPN {
-	/**
-	 * Clase principal para el cálculo del resultado de una operación con parametros RPN.
-	 * @param String commando. Un string con los valores y el símbolo que define la operación. Ejemplo: "5 5 +". 
-	 */	
+
 	private String commando;
 	private NodoPila arriba;
-	
+	/**
+	 * Clase principal para el cálculo del resultado de una operación con parametros RPN.
+	 * @param commando. Un string con los valores y el símbolo que define la operación. Ejemplo: "5 5 +". 
+	 */	
 	public RPN(String commando) {
 
 		arriba = null;
@@ -46,11 +43,6 @@ public class RPN {
 	}
 	
 	private void pusher(String[] inputItems) {
-		/**
-		 * Método que separa los diferentes items contenidos en el string que recibe el constructor y envía como parámetros a los métodos correspondientes.
-		 * @param String[] 
-		 * @return void
-		 */
 		double a, b;
 				  
         for (String item : inputItems) {
@@ -79,13 +71,12 @@ public class RPN {
         }
 	}
 	
-	
+	/**
+	 * Principal método que llama al resto de métodos privados. Estos procesan la variable commando para devolver un valor.
+	 * @return val. Número resultado de la operación introducida en el constructor.
+	 */	
 	public double resultado() {
-		/**
-		 * Método publico que llama al resto de métodos.
-		 * @params none
-		 * @return double val. Número resultado de la operación introducida en el constructor.
-		 */
+
 		String[] inputItems = commando.split(" ");
 		pusher(inputItems);
 		double val = popPila();
---
+++ doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"calculadora","c":"RPN","l":"resultado()"},{"p":"calculadora","c":"RPN","l":"RPN(String)","url":"%3Cinit%3E(java.lang.String)"}]
